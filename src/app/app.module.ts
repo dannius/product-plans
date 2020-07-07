7e8e86bbec6210469910407873b6fbb19aaef7c1
daniel <dan.sldv@gmail.com>
2020-07-07 17:01:22 +0300

    Add core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CoreModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
